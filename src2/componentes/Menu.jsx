49b42f4f9a6915e816bab1f7559b12a76f777c4e
gabyysID <gabriellyleitesocial@gmail.com>
2024-11-19 14:50:51 -0300

    Update Menu.jsx

--- src2/componentes/Menu.jsx
+++ src2/componentes/Menu.jsx
@@ -1,12 +1,52 @@
+// components/Menu.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  width: 200px;
+  background-color: #1a1a2e;
+  color: white;
+  padding: 20px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  margin: 15px 0;
+`;
 
 const Menu = () => (
-  <nav>
-    <Link to="/">Início</Link>
-    <Link to="/chamados">Chamados T.I</Link>
-    <Link to="/historico">Histórico</Link>
-  </nav>
+  <Nav>
+    <Title>Chamados de T.I</Title>
+    <List>
+      <ListItem>
+        <Link to="/form-physical" style={{ color: 'white', textDecoration: 'none' }}>Chamado para Infraestrurura</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="/form-system" style={{ color: 'white', textDecoration: 'none' }}>Chamado para Sistema</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="/user-history" style={{ color: 'white', textDecoration: 'none' }}>Histórico</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="/admin-dashboard" style={{ color: 'white', textDecoration: 'none' }}>Painel de Controle</Link>
+      </ListItem>
+    </List>
+    <div style={{ marginTop: 'auto', color: 'white' }}>
+      Emergência? Ligue ****
+    </div>
+  </Nav>
 );
 
 export default Menu;
