c3f5406e4c34c1c5debedbfc15c9747dfd49975e
gabyysID <gabriellyleitesocial@gmail.com>
2024-11-19 14:50:13 -0300

    Create FormSistema.jsx

---
+++ src2/componentes/FormSistema.jsx
@@ -0,0 +1,9 @@
+// components/FormSystem.jsx
+import React from 'react';
+import FormBase from './FormBase';
+
+const FormSystem = () => {
+  return <FormBase category="Sistema" />;
+};
+
+export default FormSystem;
