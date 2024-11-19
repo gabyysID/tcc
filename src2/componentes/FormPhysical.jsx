bb8c9675252f48d0f316c1e86523b8cdfdfae986
gabyysID <gabriellyleitesocial@gmail.com>
2024-11-19 14:49:43 -0300

    Update and rename FormPopup.jsx to FormPhysical.jsx

---
+++ src2/componentes/FormPhysical.jsx
@@ -0,0 +1,10 @@
+// components/FormPhysical.jsx
+import React from 'react';
+import FormBase from './FormBase';
+
+const FormPhysical = () => {
+  return <FormBase category="Infraestrutura" />;
+};
+
+export default FormPhysical;
+
